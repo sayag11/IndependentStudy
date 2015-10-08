ab7b67a9c4c2ad6da28e79d1a7bd5af071ff9932
sayag11 <sayag11@gmail.com>
2015-10-07 21:22:59 -0400

    test

--- ExerciseTwo/ExerciseTwo/Program.cs
+++ ExerciseTwo/ExerciseTwo/Program.cs
@@ -11,7 +11,7 @@ namespace ExerciseTwo
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Commit Successful");
         }
     }
 }
